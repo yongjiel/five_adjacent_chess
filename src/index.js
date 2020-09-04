17474eafac20be43ee68789fe5c7f4f5623a894a
Yongjie Liang <yongjiel@ualberta.ca>
2020-09-04 13:30:12 -0600

    tailwind version

--- src/index.js
+++ src/index.js
@@ -6,17 +6,16 @@ Instead with adjacent 5 same chars
 will win the game.
 This is also named in Chinese chest 'Five finger chest'.
 */
-
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Game from './Game';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Game from "./Game";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
 import rootReducers from "./rootReducer";
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import "./tailwind.output.css";
 
 const store = createStore(rootReducers, applyMiddleware(thunk));
 //const store = createStore(rootReducers)
@@ -24,7 +23,7 @@ const store = createStore(rootReducers, applyMiddleware(thunk));
 
 const App = () => (
   <Provider store={store}>
-    <Game rows='15'/>
+    <Game rows="15" />
   </Provider>
 );
 /*
@@ -34,10 +33,9 @@ ReactDOM.render(
 );
 */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
