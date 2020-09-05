ebf6cbddea54e69aa806effd15b7a24802d9dbaf
Yongjie Liang <yongjiel@ualberta.ca>
2020-09-04 22:35:19 -0600

    Change to Footer

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ will win the game.
 This is also named in Chinese chest 'Five finger chest'.
 */
 
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Game from './Game';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Game from "./Game";
+import Footer from "./Footer";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
 import rootReducers from "./rootReducer";
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(rootReducers, applyMiddleware(thunk));
 //const store = createStore(rootReducers)
@@ -24,7 +24,8 @@ const store = createStore(rootReducers, applyMiddleware(thunk));
 
 const App = () => (
   <Provider store={store}>
-    <Game rows='15'/>
+    <Game rows="15" />
+    <Footer />
   </Provider>
 );
 /*
@@ -34,10 +35,9 @@ ReactDOM.render(
 );
 */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
