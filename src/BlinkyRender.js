7aab5678390c2d21b52e818e3aefe259f25c6244
yongjiel <yongjiel@Setups-MacBook.local>
2020-09-06 15:25:38 -0600

    Added BlinkyRender to compare useEffect and useLayoutEffect

---
+++ src/BlinkyRender.js
@@ -0,0 +1,27 @@
+import React, {
+    useState,
+    useLayoutEffect,
+    useEffect
+} from 'react';
+
+const BlinkyRender = () => {
+    const [value, setValue] = useState(0);
+
+    useLayoutEffect(() => {
+        if (value === 0) {
+            setValue(10 + Math.random() * 200);
+        }
+    }, [value]);
+
+    console.log('render', value);
+
+    return (
+        <div onClick={() => setValue(0)}>
+            value: {value}
+        </div>
+    );
+};
+
+
+// Must export!
+export default BlinkyRender;
