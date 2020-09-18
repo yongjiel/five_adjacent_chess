a0e01185ebb11b8cfa68fa6ddc61c1f5818bd671
Yongjie Liang <yongjiel@ualberta.ca>
2020-09-18 11:27:22 -0600

    Added react-bootstrap to show nav bar

--- src/index.js
+++ src/index.js
@@ -23,6 +23,15 @@ import FetchDemo from "./fetchDemo";
 import FetchDemoFlaskSocketIO from "./fetchDemoFlaskSocketIO";
 import RoomApp from "./room";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const store = createStore(rootReducers, applyMiddleware(thunk));
 //const store = createStore(rootReducers)
@@ -43,25 +52,30 @@ const App = () => (
 );
 
 const nav_bar = ({ match }) => (
-  <div className="header">
-    <ul>
-      <li>
-        <Link to={"/game"}>Game</Link>
-      </li>
-      <li>
-        <Link to={"/blinkyrender"}>BlinkyRender</Link>
-      </li>
-      <li>
-        <Link to={"/threecounts"}>ThreeCounts</Link>
-      </li>
-      <li>
-        <Link to={"/fetchdemo"}>FetchDemo</Link>
-      </li>
-      <li>
-        <Link to={"/room"}>Room</Link>
-      </li>
-    </ul>
-  </div>
+  <Navbar bg="info" expand="lg">
+    <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/game">Game</Nav.Link>
+        <Nav.Link href="/blinkyrender">BlinkyRender</Nav.Link>
+        <Nav.Link href="/threecounts">ThreeCounts</Nav.Link>
+        <Nav.Link href="/fetchdemo">FetchDemo</Nav.Link>
+        <Nav.Link href="/room">Room</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
 );
 
 const game = ({ match }) => (
