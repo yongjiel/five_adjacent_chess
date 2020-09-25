ce301895f360746152aba563582fe3d010455436
Yongjie Liang <yongjiel@ualberta.ca>
2020-09-25 12:21:25 -0600

    Added error handling in axios in fetchDemo.js

--- src/fetchDemo.js
+++ src/fetchDemo.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 class FetchDemo extends React.Component {
   state = {
+    error: "",
     posts: [],
   };
 
@@ -12,15 +13,32 @@ class FetchDemo extends React.Component {
       .then((res) => {
         const posts = res.data.data.children.map((obj) => obj.data);
         this.setState({ posts });
+      })
+      .catch((error) => {
+        if (error.response) {
+          let error = "Response with Error: " + error.response;
+          this.setState({ error });
+        } else if (error.request) {
+          this.setState({ error: "No response after requeset." });
+        } else {
+          this.setState({ error: "Unknown errors" });
+        }
       });
   }
 
   render() {
-    if (this.state.posts.length === 0) {
+    if (!this.state.error && this.state.posts.length === 0) {
       return (
         <div>
           <h1>{`/r/${this.props.subreddit}`}</h1>
           <p>No posts yet</p>
+        </div>
+      );
+    } else if (!!this.state.error) {
+      return (
+        <div>
+          <h1>{`/r/${this.props.subreddit}`}</h1>
+          <p>{this.state.error}</p>
         </div>
       );
     }
